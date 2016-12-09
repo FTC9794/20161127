f965bacc7d92e65e504659b54b7be1d350c4159a
Ishaan <ishaanoberoi@gmail.com>
2016-12-09 09:57:09 -0500

    file testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FileReadWriteTest.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.util.ReadWriteFile;
+
+import org.firstinspires.ftc.robotcore.internal.AppUtil;
+import java.io.File;
+
+/**
+ * Created by Ishaan Oberoi on 12/8/2016.
+ */
+
+public class FileReadWriteTest {
+
+    static String filename = "AutoAngle.txt";
+    static File myFile = new File(filename);
+
+    static void writeToFile() {
+        ReadWriteFile.writeFile(myFile, "Jyoti");
+    }
+
+    static String voidReadFromFile() {
+        return ReadWriteFile.readFile(myFile);
+    }
+}
